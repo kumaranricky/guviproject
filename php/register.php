0710b9d134ee3b66194806fb1e6edf5cdcc9e8f8
kumaranricky <brkumaran03@gmail.com>
2023-03-11 21:55:56 +0530

    user info saved to php

--- php/register.php
+++ php/register.php
@@ -1,10 +1,15 @@
 <?php
+include("./db.php");
 
 // get form data
+try {
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
+
+$created_at = date("Y-m-d H:i:s");
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 // validate form data
 if (empty($username) || empty($email) || empty($password) || empty($confirm_password)) {
@@ -13,11 +18,11 @@ if (empty($username) || empty($email) || empty($password) || empty($confirm_pass
   $response = array('success' => false, 'message' => 'Passwords do not match.');
 } else {
   // connect to database
-  $conn = new mysqli('localhost', 'username', 'password', 'database_name');
+  $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
   // prepare query
-  $stmt = $conn->prepare('INSERT INTO users (username, email, password) VALUES (?, ?, ?)');
-  $stmt->bind_param('sss', $username, $email, $password);
+  $stmt = $conn->prepare('INSERT INTO users (username, email, password,created_at) VALUES (?, ?, ?, ?)');
+  $stmt->bind_param('ssss', $username, $email, $password_hash,$created_at);
 
   // execute query
   if ($stmt->execute()) {
@@ -30,7 +35,11 @@ if (empty($username) || empty($email) || empty($password) || empty($confirm_pass
   $stmt->close();
   $conn->close();
 }
-
+}catch(mysqli_sql_exception $e) {
+  //echo 'Message: ' .$e->getMessage();
+  $response = array('success' => false);
+  //response = array('success' => 100,'message'=>'Message: ' .$e->getMessage());
+}
 // send response to client
 header('Content-Type: application/json');
 echo json_encode($response);
